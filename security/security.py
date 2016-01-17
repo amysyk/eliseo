e69d2b3003f9896c917ed49d0aa60db33e9a9232
amysyk <andriy.mysyk@gmail.com>
2016-01-17 14:13:35 -0800

    added gpio support

--- security/security.py
+++ security/security.py
@@ -1,17 +1,20 @@
 import time
-import RPi.GPIO
+import RPi.GPIO as io
 from flask import request
 from flask import Flask
 from config import *
 
 app = Flask(__name__)
 
-RPi.GPIO.setmode(RPi.GPIO.BCM)
-RPi.GPIO.setup(IN_GARAGE_DOOR_PORT, RPi.GPIO.IN)
+io.setmode(RPi.GPIO.BCM)
+io.setup(IN_GARAGE_DOOR_PORT, io.IN, pull_up_down=io.PUD_UP)
 
 @app.route("/v1/Security/Doors/InGarageDoor/State")
 def inGarageDoorState():
-   return "closed"
-
+   if io.input(IN_GARAGE_DOOR_PORT):
+      state = "Open"
+   else
+      state = "Closed"
+   return state
 if __name__ == "__main__":
    app.run(host = '0.0.0.0', port = WEB_SERVER_PORT)
