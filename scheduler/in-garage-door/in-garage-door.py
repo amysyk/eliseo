73f86729f2b19a40294fae13815156a684c85d7e
amysyk <andriy.mysyk@gmail.com>
2016-01-18 19:09:54 -0800

    removed slack notification

--- scheduler/in-garage-door/in-garage-door.py
+++ scheduler/in-garage-door/in-garage-door.py
@@ -20,11 +20,5 @@ while True:
          "localtime": "PT" + LIGHT_ON_DURATION}
       r = requests.post(HUE_SCHEDULES_URL, data = json.dumps(payload))
  
-      #post update in slack
-      r = requests.request('GET', SLACK_URL_START + "Door to garage opened" + SLACK_URL_END)
-   elif (currentState == "Closed" and currentState != lastState and lastState !=""):
-      #post update in slack
-      r = requests.request('GET', SLACK_URL_START + "Door to garage closed" + SLACK_URL_END)
- 
    lastState = currentState
    time.sleep(SLEEP_TIME_IN_SECS)
